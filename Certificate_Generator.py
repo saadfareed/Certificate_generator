a4dc09e2b7a2f7c6cb7aae191beb05c9033892f1
saadfareed <saadfareed632@gmail.com>
2021-08-24 06:56:06 +0500

    Text size changes

--- Certificate_Generator.py
+++ Certificate_Generator.py
@@ -1,16 +1,13 @@
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
-
-data = pd.read_excel (r'C:\Users\Rahul sinha\Desktop\Python Learning Jupyter\gen.xlsx') 
+#process
+data = pd.read_csv (r'Git and Git-Hub.csv') 
 name_list = data["Name"].tolist() 
 for i in name_list:
-    im = Image.open(r'C:\Users\Rahul sinha\Desktop\Python Learning Jupyter\new.jpg')
+    im = Image.open(r'Untitled-1.jpg')
     d = ImageDraw.Draw(im)
-    location = (100, 398)
-    text_color = (0, 137, 209)
-    font = ImageFont.truetype("arial.ttf", 120)
-    d.text(location, i, fill = text_color, font = font)
+    fon = ImageFont.truetype("arial.ttf", 60)
+    location = (700, 650)
+    text_color = (0, 46, 99)
+    d.text(location, i, fill = text_color,font=fon ,stroke_width=2)
     im.save("certificate_" + i + ".pdf")
-
-
-

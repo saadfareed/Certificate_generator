37d4049215a1a1643251b89e792136edfbae9ac5
saadfareed <saadfareed282681@gmail.com>
2021-08-25 10:44:42 +0500

    New Template

--- Certificate_Generator.py
+++ Certificate_Generator.py
@@ -11,10 +11,21 @@ name_list = data["Name"].tolist()
 for i in name_list:
     im = Image.open(r'Template.jpg')
     d = ImageDraw.Draw(im)
+    #Attendee name
     fon = ImageFont.truetype("arial.ttf", 60)
-    location = (700, 645)
-    text_color = (0, 46, 99)
-    d.text(location, i, fill = text_color,font=fon ,stroke_width=1)
+    location = (675, 645)
+    text_color = (0, 30, 49)
+    #location for Attendee name
+    d.text(location, i, fill = text_color,font=fon ,stroke_width=2)
+
+    #my name signature
+    fox = ImageFont.truetype("PWSignaturetwo.ttf", 50)
+    locat = (85, 1000)
+    txt_color = (0, 0, 0)
+    l="Muhammad Saad Fareed"
+    #location for my signature
+    d.text(locat, l , fill = txt_color,font=fox ,stroke_width=1)
+    #Saving my certificate
     cert_dir = '/home/sadi/Certificate generator/certificates/'
     filee=cert_dir+"certificate_" + i + ".pdf"
     im.save(filee)
